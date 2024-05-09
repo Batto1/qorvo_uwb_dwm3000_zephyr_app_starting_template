c86b5963f2e29a6f55bb4faf125bc9f069985e85
Hikmet Batuhan Görgülü <bhan2357@gmail.com>
2024-05-09 17:35:20 +0300

    doesn't build for DWS3000 yet, small mistakes will be fixed

--- app/core/main.c
+++ app/core/main.c
@@ -1,12 +1,154 @@
-/*
- * Copyright (c) 2012-2014 Wind River Systems, Inc.
- *
- * SPDX-License-Identifier: Apache-2.0
+/**
+ * @file    main.c
+ * @brief   Simple TX example code, directly copied from DWM3000 examples - ex_01a_simple_tx. This was an example code for testing out if build will fail or not.
+ * @note 	  
+ * 
  */
 
 #include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/sys/printk.h>
+
+#include <qorvo_uwb_dwm3000/decadriver/deca_device_api.h>
+#include <qorvo_uwb_dwm3000/decadriver/deca_regs.h>
+#include <qorvo_uwb_dwm3000/platform/deca_spi.h>
+#include <qorvo_uwb_dwm3000/platform/port.h>
+#include <qorvo_uwb_dwm3000/shared_data/shared_defines.h>
+
+
+LOG_MODULE_REGISTER(simple_tx);
+
+/* Example application name */
+#define APP_NAME "SIMPLE TX v1.0"
+
+/* Default communication configuration. We use default non-STS DW mode. */
+static dwt_config_t config = {
+    .chan            = 5,               /* Channel number. */
+    .txPreambLength  = DWT_PLEN_128,    /* Preamble length. Used in TX only. */
+    .rxPAC           = DWT_PAC8,        /* Preamble acquisition chunk size. Used in RX only. */
+    .txCode          = 9,               /* TX preamble code. Used in TX only. */
+    .rxCode          = 9,               /* RX preamble code. Used in RX only. */
+    .sfdType         = DWT_SFD_DW_8,    /* 0 to use standard 8 symbol SFD */
+    .dataRate        = DWT_BR_6M8,      /* Data rate. */
+    .phrMode         = DWT_PHRMODE_STD, /* PHY header mode. */
+    .phrRate         = DWT_PHRRATE_STD, /* PHY header rate. */
+    .sfdTO           = (129 + 8 - 8),   /* SFD timeout */
+    .stsMode         = DWT_STS_MODE_OFF,
+    .stsLength       = DWT_STS_LEN_64,  /* STS length, see allowed values in Enum dwt_sts_lengths_e */
+    .pdoaMode        = DWT_PDOA_M0      /* PDOA mode off */
+};
+
+/* The frame sent in this example is an 802.15.4e standard blink. It is a 12-byte frame composed of the following fields:
+ *     - byte 0: frame type (0xC5 for a blink).
+ *     - byte 1: sequence number, incremented for each new frame.
+ *     - byte 2 -> 9: device ID, see NOTE 1 below.
+ */
+static uint8_t tx_msg[] = {0xC5, 0, 'D', 'E', 'C', 'A', 'W', 'A', 'V', 'E'};
+
+/* Index to access to sequence number of the blink frame in the tx_msg array. */
+#define BLINK_FRAME_SN_IDX 1
+
+#define FRAME_LENGTH    (sizeof(tx_msg)+FCS_LEN) //The real length that is going to be transmitted
+
+/* Inter-frame delay period, in milliseconds. */
+#define TX_DELAY_MS 500
+
+/* Values for the PG_DELAY and TX_POWER registers reflect the bandwidth and
+ * power of the spectrum at the current temperature. These values can be
+ * calibrated prior to taking reference measurements.
+ * See NOTE 2 below. */
+extern dwt_txconfig_t txconfig_options;
+
+
+
+int app_main(void);
+
 
 void main(void)
 {
-	printk("Hello World! %s\n", CONFIG_BOARD);
+	(void)app_main();
+}
+
+
+/**
+ * Application entry point.
+ */
+int app_main(void)
+{
+    /* Display application name. */
+    LOG_INF(APP_NAME);
+
+    /* Configure SPI rate, DW3000 supports up to 38 MHz */
+    port_set_dw_ic_spi_fastrate();
+
+    /* Reset DW IC */
+    reset_DWIC(); /* Target specific drive of RSTn line into DW IC low for a period. */
+
+    /* Time needed for DW3000 to start up (transition from INIT_RC to IDLE_RC,
+     *( or could wait for SPIRDY event) */
+    Sleep(2);
+
+    /* Need to make sure DW IC is in IDLE_RC before proceeding */
+    while (!dwt_checkidlerc()) { /* spin */ };
+
+    if (dwt_initialise(DWT_DW_INIT) == DWT_ERROR) {
+        LOG_ERR("INIT FAILED");
+        while (1) { /* spin */ };
+    }
+
+    /* Enabling LEDs here for debug so that for each TX the D1 LED will flash
+     * on DW3000 red eval-shield boards. */
+    dwt_setleds(DWT_LEDS_ENABLE | DWT_LEDS_INIT_BLINK) ;
+
+    /* Configure DW IC. See NOTE 5 below. */
+    /* If the dwt_configure returns DWT_ERROR either the PLL or RX calibration
+     * has failed the host should reset the device */
+    if (dwt_configure(&config))  {
+        LOG_ERR("CONFIG FAILED");
+        while (1) { /* spin */ };
+    }
+
+    /* Configure the TX spectrum parameters (power PG delay and PG Count) */
+    dwt_configuretxrf(&txconfig_options);
+
+    LOG_INF("Sending started");
+
+    /* Loop forever sending frames periodically. */
+    while (1) {
+        {
+            char len[9];
+            sprintf(len, "len %d", FRAME_LENGTH-FCS_LEN);
+            LOG_HEXDUMP_INF((char*)&tx_msg, sizeof(tx_msg), (char*) &len);
+        }
+
+        /* Write frame data to DW IC and prepare transmission. See NOTE 3 below.*/
+        dwt_writetxdata(FRAME_LENGTH-FCS_LEN, tx_msg, 0); /* Zero offset in TX buffer. */
+
+        /* In this example since the length of the transmitted frame does not change,
+         * nor the other parameters of the dwt_writetxfctrl function, the
+         * dwt_writetxfctrl call could be outside the main while(1) loop.
+         */
+        dwt_writetxfctrl(FRAME_LENGTH, 0, 0); /* Zero offset in TX buffer, no ranging. */
+
+        /* Start transmission. */
+        dwt_starttx(DWT_START_TX_IMMEDIATE);
+
+        /* Poll DW IC until TX frame sent event set. See NOTE 4 below.
+         * STATUS register is 4 bytes long but, as the event we are looking at
+         * is in the first byte of the register, we can use this simplest API
+         * function to access it.*/
+        while (!(dwt_read32bitreg(SYS_STATUS_ID) & SYS_STATUS_TXFRS_BIT_MASK))
+        { /* spin */ };
+
+        /* Clear TX frame sent event. */
+        dwt_write32bitreg(SYS_STATUS_ID, SYS_STATUS_TXFRS_BIT_MASK);
+
+        //LOG_INF("TX Frame Sent");
+
+        /* Execute a delay between transmissions. */
+        Sleep(TX_DELAY_MS);
+
+        /* Increment the blink frame sequence number (modulo 256). */
+        tx_msg[BLINK_FRAME_SN_IDX]++;
+    }
 }
